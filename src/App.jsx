59f544233116c029633f6b0a6a1b9f9e6694e37c
Gunadi PS <gndsulistiawan@gmail.com>
2023-05-04 02:22:23 +0700

    menambahkan kondisi

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
+// import Counter from './components/Counter';
 import { useState } from 'react';
+
+import React from 'react';
 
 function App() {
 	const [counter, setCount] = useState(0);
+	const [warningMessage, setWarningMessage] = React.useState(false);
 
+	const checkValueCondition = (value, callback) => {
+		if (value >= 1 && value <= 10) {
+			setWarningMessage(false);
+			callback();
+		}
+		setWarningMessage(true);
+	};
 	const plusFunction = () => {
 		const value = counter + 1;
-		setCount(value);
+		// setCount(value);
+		checkValueCondition(value, function () {
+			setCount(value);
+		});
 	};
 
 	const minFunction = () => {
 		const value = counter - 1;
-		setCount(value);
+		// setCount(value);
+		checkValueCondition(value, function () {
+			setCount(value);
+		});
 	};
 
 	return (
 		<>
+			<div className="text-center my-5">{warningMessage}</div>
 			<div className="flex flex-col justify-center items-center gap-5">
 				<div className="text-center">
 					<p>{counter}</p>
 				</div>
 				<div className="flex gap-5">
-					<button className="w-[100px] border-2" onClick={plusFunction}>
-						+
-					</button>
 					<button className="w-[100px] border-2" onClick={minFunction}>
 						-
+					</button>
+					<button className="w-[100px] border-2" onClick={plusFunction}>
+						+
 					</button>
 				</div>
 			</div>
---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+
+// import React from "react";
+
+// function WarnMessage(){
+//     const [warningMessage, setWarningMessage] = React.useState('hello')
+
+//     return <>
+//     <div className="text-center my-5">
+//         {warningMessage}
+//     </div>
+//     </>
+// }
+
+// export default WarnMessage
