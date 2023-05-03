e15a62f0b410634cce8fd0f87562b71ed56981af
Gunadi PS <gndsulistiawan@gmail.com>
2023-05-03 16:14:23 +0700

    create counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [counter, setCount] = useState(0);
+
+	const plusFunction = () => {
+		const value = counter + 1;
+		setCount(value);
+	};
+
+	const minFunction = () => {
+		const value = counter - 1;
+		setCount(value);
+	};
 
 	return (
 		<>
 			<div className="flex flex-col justify-center items-center gap-5">
 				<div className="text-center">
-					<p>{count}</p>
+					<p>{counter}</p>
 				</div>
-				<div className="card flex gap-5">
-					<button className="w-[100px] border-2" onClick={() => setCount((count) => count + 1)}>
+				<div className="flex gap-5">
+					<button className="w-[100px] border-2" onClick={plusFunction}>
 						+
 					</button>
-					<button className="w-[100px] border-2" onClick={() => setCount((count) => count - 1)}>
+					<button className="w-[100px] border-2" onClick={minFunction}>
 						-
 					</button>
 				</div>
